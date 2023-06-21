21760052fa73f6ca8aa0a42ac89f3a1772b7fdd3
smile0902 <rladjstn@dgsw.hs.kr>
2023-06-22 02:57:36 +0900

    commit

--- na_a/src/router/index.tsx
+++ na_a/src/router/index.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/Auth/LoginPage";
+import SignUpPage from "../pages/Auth/SignUpPage";
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
     </Routes>
   );
 }
