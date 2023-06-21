ac8ee5f56068c0551194e64ba50d86bfc071a50a
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-06-22 03:02:31 +0900

    hotfix

--- na_a/src/components/Common/Auth/SignUp/index.tsx
+++ na_a/src/components/Common/Auth/SignUp/index.tsx
@@ -1,42 +1,40 @@
 import * as S from "./style";
-import uploadProfile from "../../../../assets/uploadProfile.svg"
+import uploadProfile from "../../../../assets/uploadProfile.svg";
 import { useNavigate } from "react-router-dom";
 
 function SignUp() {
-    const navigate = useNavigate();
-    return (
-      <S.SignUpCenter>
+  const navigate = useNavigate();
+  return (
+    <S.SignUpCenter>
+      <div>
+        <S.SignUpCenter>
+          <S.SignUpImgDiv>
+            <input id="file" type="file" />
+            <label htmlFor="file">
+              <img src={uploadProfile} alt="" />
+            </label>
+          </S.SignUpImgDiv>
+        </S.SignUpCenter>
         <div>
-          <S.SignUpCenter>
-            <S.SignUpImgDiv>
-              <input id="file" type="file" />
-              <label htmlFor="file">
-                <img src={uploadProfile} alt="" />
-              </label>
-            </S.SignUpImgDiv>
-          </S.SignUpCenter>
-          <div>
-            <S.SignUpP>이름</S.SignUpP>
-            <S.SignUpInputBox type="text" />
-          </div>
-          <div>
-            <S.SignUpP>아이디</S.SignUpP>
-            <S.SignUpInputBox type="text" />
-          </div>
-          <div>
-            <S.SignUpP>비밀번호</S.SignUpP>
-            <S.SignUpInputBox type="password" />
-          </div>
-          <S.SignUpButton>회원가입</S.SignUpButton>
-          <div>
-            <S.SignUpSpan>
-              이미 계정이 있으신가요?
-            </S.SignUpSpan>
-            <S.SignUpDiv onClick={()=>navigate("/login")}>로그인</S.SignUpDiv>
-          </div>
+          <S.SignUpP>이름</S.SignUpP>
+          <S.SignUpInputBox type="text" />
         </div>
-      </S.SignUpCenter>
-    );
-  }
-  
-  export default SignUp;
+        <div>
+          <S.SignUpP>아이디</S.SignUpP>
+          <S.SignUpInputBox type="text" />
+        </div>
+        <div>
+          <S.SignUpP>비밀번호</S.SignUpP>
+          <S.SignUpInputBox type="password" />
+        </div>
+        <S.SignUpButton>회원가입</S.SignUpButton>
+        <div>
+          <S.SignUpSpan>이미 계정이 있으신가요?</S.SignUpSpan>
+          <S.SignUpDiv onClick={() => navigate("/login")}>로그인</S.SignUpDiv>
+        </div>
+      </div>
+    </S.SignUpCenter>
+  );
+}
+
+export default SignUp;
