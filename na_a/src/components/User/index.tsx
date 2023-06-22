9693ded3c3bdb99991438183847e63b6c558d41b
smile0902 <rladjstn@dgsw.hs.kr>
2023-06-22 20:01:14 +0900

    commit

--- na_a/src/components/User/index.tsx
+++ na_a/src/components/User/index.tsx
@@ -1,6 +1,29 @@
 import { Container } from "../style";
+import userProfile from "../../assets/userProfile.svg";
 import * as S from "./style";
 
 export default function User() {
-  return <Container>유저페이지</Container>;
+  return <Container>
+    <S.UserCenter>
+      <div>
+        <S.UserCenter>
+        <S.UserImgDiv>
+          <img src={userProfile} alt="" />
+        </S.UserImgDiv>
+        </S.UserCenter>
+        <div>
+          <S.UserProfileDiv>
+            <div>
+            <S.UserSpan>이름</S.UserSpan>
+            <S.UserNameSpan>이름</S.UserNameSpan>
+            </div>
+            <div>
+            <S.UserSpan>아이디</S.UserSpan>
+            <S.UserIdSpan>아이디</S.UserIdSpan>
+            </div>
+          </S.UserProfileDiv>
+        </div>
+      </div>
+    </S.UserCenter>
+  </Container>;
 }
--- na_a/src/components/User/style.ts
+++ na_a/src/components/User/style.ts
@@ -1,1 +1,60 @@
 import styled from "styled-components";
+
+export const UserCenter = styled.div`
+    display: flex;
+    justify-content : center;
+`;
+
+export const UserImgDiv = styled.div`
+    width: 100px;
+    height: 100px;
+    margin-top: 125px;
+`;
+
+export const UserProfileDiv = styled.div`
+    width: 350px;
+    height: 116px;
+    background-color: #FFDF65;
+    border-radius: 10px;
+    margin-top: 20px;
+`;
+
+export const UserSpan = styled.p`
+    font-size: 15px;
+    display: inline-block;
+    margin-left: 30px;
+    clear: #5E5E5E;
+`;
+
+export const UserNameSpan = styled.p`
+    font-size: 13px;
+    display: inline-block;
+    margin-top: 28px;
+    margin-bottom: 34px;
+    margin-left: 15px;
+    clear: #5E5E5E;
+`;
+
+export const UserIdSpan = styled.p`
+    font-size: 12px;
+    display: inline-block;
+    margin-left: 15px;
+`;
+
+export const UserButton = styled.button`
+    width: 100px;
+    height: 25px;
+    border: none;
+    border-radius: 50px;
+    background-color: #FFDF65;
+`;
+
+export const UserNameInput = styled.input`
+    width: 40px;
+    height: 25px;
+    outline: none;
+    border: none;
+    margin-top: 15px;
+    margin-bottom: 15px;
+`;
+
